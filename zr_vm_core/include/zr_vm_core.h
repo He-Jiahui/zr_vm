1e635bcb3fbc88582c0a01e432e7d265ce20a7c7
He-Jiahui <814111601@qq.com>
2025-06-04 03:00:51 +0800

    Init Project

---
+++ zr_vm_core/include/zr_vm_core.h
@@ -0,0 +1,13 @@
+//
+// Created by HeJiahui on 2025/6/4.
+//
+
+#ifndef ZR_VM_CORE_H
+#define ZR_VM_CORE_H
+
+
+
+void Hello();
+
+
+#endif //ZR_VM_CORE_H
---
+++ zr_vm_core/src/zr_vm_core.c
@@ -0,0 +1,11 @@
+//
+// Created by HeJiahui on 2025/6/4.
+//
+#include "zr_vm_core.h"
+
+#include "zr_vm_version_info.h"
+
+#include <stdio.h>
+void Hello(){
+    printf("zr_vm version is %s\n", ZR_VM_VERSION);
+}
