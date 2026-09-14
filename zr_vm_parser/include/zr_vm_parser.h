8aaa0d13fba5aaed87846e33bb014ac044b723fa
He-Jiahui <814111601@qq.com>
2026-09-14 11:15:22 +0800

    feat(ssa): export array lowering contract

--- zr_vm_parser/include/zr_vm_parser.h
+++ zr_vm_parser/include/zr_vm_parser.h
@@ -11,6 +11,7 @@
 #include "zr_vm_parser/ast.h"
 #include "zr_vm_parser/compiler.h"
 #include "zr_vm_parser/exec_ir_builder.h"
+#include "zr_vm_parser/exec_ir_array_lowering.h"
 #include "zr_vm_parser/exec_ir_binding_facts.h"
 #include "zr_vm_parser/exec_ir_layout_visibility.h"
 #include "zr_vm_parser/semantic.h"
